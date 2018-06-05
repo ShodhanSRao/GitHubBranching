92bae10b18e3f19731b0a33c5a267af7d5875b1b
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 15:05:07 +0530

    taken pull from thailand-cherry pick

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -1,17 +1,29 @@
 package com.schneider.githubtest;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    int variable = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"This is an indonesia build",Toast.LENGTH_SHORT)
-                .show();
+       
+    }
+
+    public  Locale getCurrentLocale() {
+        Locale locale = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            locale = this.getResources().getConfiguration().getLocales().get(0);
+        } else {
+            locale = this.getResources().getConfiguration().locale;
+        }
+        return locale;
     }
 }
