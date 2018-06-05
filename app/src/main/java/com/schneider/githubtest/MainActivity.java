42573694d28b854337be37003036a3b0a794296b
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 15:04:04 +0530

    added locale method in thailand

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.schneider.githubtest;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"This is an thailand build",Toast.LENGTH_SHORT)
                 .show();
+    }
+
+    public  Locale getCurrentLocale() {
+        Locale locale = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            locale = this.getResources().getConfiguration().getLocales().get(0);
+        } else {
+            locale = this.getResources().getConfiguration().locale;
+        }
+        return locale;
     }
 }
