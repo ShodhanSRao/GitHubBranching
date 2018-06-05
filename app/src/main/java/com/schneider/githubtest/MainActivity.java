e05e9a96b5cf75ea5a16f7e40102d9669d7e92bc
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 14:29:26 +0530

    changed the string to indonesia

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"This is an Indonesia build",Toast.LENGTH_SHORT)
+        Toast.makeText(this,"This is an indonesia build",Toast.LENGTH_SHORT)
                 .show();
     }
 }
