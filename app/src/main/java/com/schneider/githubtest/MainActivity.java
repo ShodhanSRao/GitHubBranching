ad2ee485d57e68a341386d43a3fe17e154520998
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 14:31:35 +0530

    added a variable in indonesia build

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int variable = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
