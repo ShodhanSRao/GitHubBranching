331b082b95c73060a0d59a7b38214b1012d9835e
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 14:52:08 +0530

    changed sentence

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"This is an Indonesia build",Toast.LENGTH_SHORT)
+        Toast.makeText(this,"This is an india build",Toast.LENGTH_SHORT)
                 .show();
     }
 }
