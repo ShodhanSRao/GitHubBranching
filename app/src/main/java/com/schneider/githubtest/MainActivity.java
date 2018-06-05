eeaa75321ad22e9bb510cb6cc0e62799a386e14a
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 14:55:10 +0530

    Text changes in india

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"This is an Indonesia build",Toast.LENGTH_SHORT)
         Toast.makeText(this,"This is an india build",Toast.LENGTH_SHORT)
                 .show();
     }
