6245f06a4348c3434413136407ad9eafbbaa7977
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 15:30:22 +0530

    commited changes to thailand

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.schneider.githubtest;
 
 import android.os.Build;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,13 +19,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public  Locale getCurrentLocale() {
-        Locale locale = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            locale = this.getResources().getConfiguration().getLocales().get(0);
-        } else {
-            locale = this.getResources().getConfiguration().locale;
+ 
+
+    public static String getFormattedDate(String date){
+        try {
+            SimpleDateFormat sdfdate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(sdfdate.parse(date));
+            SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
+            String formattedDate = sdf.format(calendar.getTime());
+            return formattedDate;
         }
-        return locale;
+        catch (Exception ex){
+            ex.printStackTrace();
+            return "";
+        }
     }
 }
