8215c377e42a32cd06680512e869c8cb0a3565a9
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 15:22:33 +0530

    added formatted date function in india

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
             locale = this.getResources().getConfiguration().locale;
         }
         return locale;
+    }
+
+    public static String getFormattedDate(String date){
+        try {
+            SimpleDateFormat sdfdate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(sdfdate.parse(date));
+            SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
+            String formattedDate = sdf.format(calendar.getTime());
+            return formattedDate;
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            return "";
+        }
     }
 }
