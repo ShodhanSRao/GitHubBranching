024f1b8acb98883b600452e0b6f0cc0fa977853c
ShodhanSRao <shodhans01@gmail.com>
2018-06-05 15:30:22 +0530

    changes from thailand

--- app/src/main/java/com/schneider/githubtest/MainActivity.java
+++ app/src/main/java/com/schneider/githubtest/MainActivity.java
@@ -3,7 +3,7 @@ package com.schneider.githubtest;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,18 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
     }
 
-    public  Locale getCurrentLocale() {
-        Locale locale = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            locale = this.getResources().getConfiguration().getLocales().get(0);
-        } else {
-            locale = this.getResources().getConfiguration().locale;
-        }
-        return locale;
-    }
+ 
 
     public static String getFormattedDate(String date){
         try {
